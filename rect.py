93a52f76c8bb5e965bd05a03c7079cec0005b474
Vipul Sharma <vipul.sharma20@gmail.com>
2016-01-10 06:44:33 +0530

    added rectifying-coordinates script

---
+++ rect.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def rectify(h):
+        h = h.reshape((4,2))
+        hnew = np.zeros((4,2),dtype = np.float32)
+
+        add = h.sum(1)
+        hnew[0] = h[np.argmin(add)]
+        hnew[2] = h[np.argmax(add)]
+
+        diff = np.diff(h,axis = 1)
+        hnew[1] = h[np.argmin(diff)]
+        hnew[3] = h[np.argmax(diff)]
+
+        return hnew
